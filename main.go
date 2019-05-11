ac8320dd821398c25a7366ad26f664ff3ec3c550
dora <m5i2n2o@gmail.com>
2019-05-11 19:42:39 +0900

    typo

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func fetch() *feeder.Feed {
 }
 
 func writeFeed(path string, body string) error {
-	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|O_TRUNC, 0644)
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
